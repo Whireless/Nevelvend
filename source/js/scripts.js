9cc00b21d9ab07d207689d912418281ab0dfdf0c
Александр <alextrexen@gmail.com>
2021-10-23 16:20:42 +0300

    Мобильная версия v1.0

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,8 +1,54 @@
-const navMain = document.querySelector('.main-nav__list');
-const navBtn = document.querySelector('.main-nav__menu-button');
-const navHead = document.querySelector('.main-nav__button-block');
+'use strict';
+
+const navMain = document.querySelector('.main-nav__list'),
+      navBtn = document.querySelector('.main-nav__menu-button'),
+      navHead = document.querySelector('.main-nav__button-block');
+
+const ordBtn1 = document.querySelector('.order__title--specific'),
+      ordBtn2 = document.querySelector('.order__title--box'),
+      ordBtn3 = document.querySelector('.order__title--buy'),
+      zone1 = document.querySelector('.order__specifications'),
+      zone2 = document.querySelector('.order__box'),
+      zone3 = document.querySelector('.order__buy');
+
+
+// Мобильное меню
 
 navBtn.addEventListener('click', function() {
   navMain.classList.toggle('main-nav__list--open')
   navHead.classList.toggle('main-nav__button-block--open')
+});
+
+// Кнопки в блоке заказа
+
+zone1.classList.add('order__specifications--active');
+
+ordBtn1.addEventListener('click', function() {
+  ordBtn1.classList.add('order__title--specific--active')
+  ordBtn2.classList.remove('order__title--box--active')
+  ordBtn3.classList.remove('order__title--buy--active')
+
+  zone1.classList.add('order__specifications--active')
+  zone2.classList.remove('order__box--active')
+  zone3.classList.remove('order__buy--active')
+});
+
+ordBtn2.addEventListener('click', function() {
+  ordBtn2.classList.add('order__title--box--active')
+  ordBtn1.classList.remove('order__title--specific--active')
+  ordBtn3.classList.remove('order__title--buy--active')
+
+  zone2.classList.add('order__box--active')
+  zone1.classList.remove('order__specifications--active')
+  zone3.classList.remove('order__buy--active')
+});
+
+ordBtn3.addEventListener('click', function() {
+  ordBtn3.classList.add('order__title--buy--active')
+  ordBtn1.classList.remove('order__title--specific--active')
+  ordBtn2.classList.remove('order__title--box--active')
+
+  zone3.classList.add('order__buy--active')
+  zone1.classList.remove('order__specifications--active')
+  zone2.classList.remove('order__box--active')
 });
