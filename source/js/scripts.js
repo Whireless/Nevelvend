5bac9e95cc18412a2859e5fd5d9f710c27fbccc1
Александр <alextrexen@gmail.com>
2023-08-22 16:50:21 +0300

    Общая оптимизация

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -9,10 +9,15 @@ const navHead = nav.querySelector('.main-nav__button-block'),
 const order = page.querySelector('.order'),
       orderButtons = order.querySelectorAll('.order__subtitle'),
       orderBlocks = order.querySelectorAll('.order__info'),
+      amountSmall = order.querySelector('.order__box-number--small'),
+      smlTotal = order.querySelector('.order__small-total'),
       smlBtnUp = order.querySelector('.order__button--small-up'),
       smlBtnDown = order.querySelector('.order__button--small-down'),
+      amountBig = order.querySelector('.order__box-number--big'),
+      bigTotal = order.querySelector('.order__big-total'),
       bigBtnUp = order.querySelector('.order__button--big-up'),
-      bigBtnDown = order.querySelector('.order__button--big-down');
+      bigBtnDown = order.querySelector('.order__button--big-down'),
+      totalInfo = order.querySelector('.order__total-price .blue-text');
 
 // Мобильное меню
 navBtn.addEventListener('click', function() {
@@ -42,4 +47,62 @@ for (let i = 0; i < orderButtons.length; i++) {
       }
     }
   })
+};
+
+// Сумма заказа
+let bigPrice = 0;
+let smallPrice = 0;
+let totalPrice = 0;
+let amountS = 0;
+let amountB = 0;
+
+smlBtnUp.onclick = () => {
+  amountS++;
+  smlBtnDown.disabled = false;
+  smallPrice += 750;
+  totalPrice += 750;
+  amountSmall.textContent = amountS;
+  smlTotal.textContent = `${smallPrice} руб.`;
+  totalInfo.textContent = `${totalPrice} руб.`;
+};
+
+smlBtnDown.onclick = () => {
+  amountS--;
+  if (amountS === 0) {
+    smlBtnDown.disabled = true;
+  }
+  smallPrice -= 750;
+  totalPrice -= 750;
+  amountSmall.textContent = amountS;
+  smlTotal.textContent = `${smallPrice} руб.`;
+  totalInfo.textContent = `${totalPrice} руб.`;
+};
+
+bigBtnUp.onclick = () => {
+  amountB++;
+  bigBtnDown.disabled = false;
+  bigPrice += 6000;
+  totalPrice += 6000;
+  amountBig.textContent = amountB;
+  bigTotal.textContent = `${bigPrice} руб.`;
+  totalInfo.textContent = `${totalPrice} руб.`;
+};
+
+bigBtnDown.onclick = () => {
+  amountB--;
+  if (amountB === 0) {
+    bigBtnDown.disabled = true;
+  }
+  bigPrice -= 6000;
+  totalPrice -= 6000;
+  amountBig.textContent = amountB;
+  bigTotal.textContent = `${bigPrice} руб.`;
+  totalInfo.textContent = `${totalPrice} руб.`;
+};
+
+window.onload = () => {
+  amountS = 0;
+  amountB = 0;
+  amountS === 0 ? smlBtnDown.disabled = true : smlBtnDown.disabled = false;
+  amountB === 0 ? bigBtnDown.disabled = true : bigBtnDown.disabled = false;
 };
